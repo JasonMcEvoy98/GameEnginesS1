3165df675158f9469bfdc2a7ca23bf3fadf8c695
Jason McEvoy <C17317363@mydit.ie>
2019-12-13 16:40:03 +0000

    Updates to the Visual Script

--- GameEnginesProjectS1/Assets/Scripts/SoundVisual.cs
+++ GameEnginesProjectS1/Assets/Scripts/SoundVisual.cs
@@ -14,14 +14,30 @@ public class SoundVisual : MonoBehaviour
     private float[] spectrum;
     private float sampleRate;
 
+    private Transform[] visualList;
+    private float[] visualScale;
+    private int amnVisual = 10;
+
     private void Start()
     {
         source = GetComponent<AudioSource>();
         samples = new float[SAMPLE_SIZE];
         spectrum = new float[SAMPLE_SIZE];
         sampleRate = AudioSettings.outputSampleRate;
+
+        SpawnLine(); 
     }
-    
+    private void SpawnLine()
+    {
+        visualScale = new float[amnVisual];
+
+    for (int i = 0; i < amnVisual; i++)
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube) as GameObject;
+            visualList[i] = go.transform;
+            visualList[i].position = Vector3.right * i;
+        }
+            }
     private void Update()
     {
         AnalyzeSound();
